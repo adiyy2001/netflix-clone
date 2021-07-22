a3d5a4a4199bdbaaa7485840bd88f5e2e524d8cf
adiyy2001 <adiy@onet.pl>
2021-07-23 00:11:48 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { FooterContainer } from "./containers/footer.container";
 import { JumbotronContainer } from "./containers/jumbotron.container";
 
 export default function App() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 }
