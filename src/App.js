09ce254302ea181edd8cc478322aa8a70e4684bb
adiyy2001 <adiy@onet.pl>
2021-07-24 12:35:11 +0200

    add faq section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { FooterContainer } from "./containers/footer.container";
 import { JumbotronContainer } from "./containers/jumbotron.container";
+import { FaqsContainer } from "./containers/faqs.container";
 
 export default function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
---
+++ src/components/accordion/accordion.component.jsx
@@ -0,0 +1,64 @@
+import React, { useState, createContext, useContext } from "react";
+import {
+  Container,
+  Frame,
+  Title,
+  Item,
+  Inner,
+  Header,
+  Body,
+} from "./accordion.style";
+
+const ToggleContext = createContext();
+
+export default function Accordion({ children, ...restProps }) {
+  return (
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>
+  );
+}
+
+Accordion.Title = function AccordionTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+  return <Frame {...restProps}>{children}</Frame>;
+};
+
+Accordion.Header = function AccordionHeader({ children, ...restProps }) {
+  const { toggleShow, setToggleShow } = useContext(ToggleContext);
+  return (
+    <Header
+      onClick={() => setToggleShow((toggleShow) => !toggleShow)}
+      {...restProps}
+    >
+      {children}
+      {toggleShow ? (
+        <img src="/images/icons/close-slim.png" alt="Close" />
+      ) : (
+        <img src="/images/icons/add.png" alt="Open" />
+      )}
+    </Header>
+  );
+};
+
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+  const [toggleShow, setToggleShow] = useState(false);
+  return (
+    <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
+      <Item {...restProps}>{children}</Item>;
+    </ToggleContext.Provider>
+  );
+};
+
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+  const { toggleShow } = useContext(ToggleContext);
+
+  return (
+    <Body className={toggleShow ? "open" : "closed"} {...restProps}>
+      <span>{children}</span>
+    </Body>
+  );
+};
