9c306d1b8dcb34e2e66ab73dca97eb18cf491d51
adiyy2001 <adiy@onet.pl>
2021-07-26 18:00:24 +0200

    add opt-form

--- src/containers/faqs.container.jsx
+++ src/containers/faqs.container.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import faqsData from "../fixtures/faqs.json";
 import { Accordion } from "../components";
+import { OptForm } from "../components";
+
 export function FaqsContainer() {
   return (
     <Accordion>
@@ -12,6 +14,14 @@ export function FaqsContainer() {
         </Accordion.Item>
       ))}
       <Accordion.Item></Accordion.Item>
+      <OptForm>
+        <OptForm.Input placeholder="Email address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership.
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
