2ff2c92b5dfaec9bb82766cabd863a38dfcf0021
adiyy2001 <adiy@onet.pl>
2021-07-27 22:45:10 +0200

    create custom hook for pulling data from firestore

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import useContent from "../hooks/content";
 export default function Browse() {
+  const { series } = useContent("series");
+  const { films } = useContent("films");
   return <></>;
 }
